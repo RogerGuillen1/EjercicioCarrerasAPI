5b4015ed05cf3e1d15fee22a80097a628e3ab466
Marcel Jimenez <marceljg04@gmail.com>
2025-10-01 09:46:00 +0100

    insert hecho

--- Carrera.py
+++ Carrera.py
@@ -1,8 +1,13 @@
 class Carrera:
     def __init__(self, pNombre):
         self.__nombre = pNombre
-    def insert(self, mycursor):
-        print("insertar")
+    
+    def insert(self, mycursor, mydb):
+        sql = "INSERT INTO carrera (nombre) VALUES (%s)"
+        val = (self.__nombre)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(f"Carrera '{self.__nombre}' insertada correctamente.")
     def see(self, mycursor):
         mycursor.execute("SELECT * FROM carreras")
         carreras = mycursor.fetchall()
