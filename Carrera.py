c18aa48eccd970cbb0fbadf768336c43b47ea67e
Roger <rogguicam@gmail.com>
2025-10-01 10:23:20 +0200

    classssse creada

---
+++ Carrera.py
@@ -0,0 +1,11 @@
+class Carrera:
+    def __init__(self, pNombre):
+        self.__nombre = pNombre
+    def insert(self):
+        print("insertar")
+    def see(self):
+        print("consultar")
+    def update(self):
+        print("actualizar")
+    def delete(self):
+        print("pues esxo")
